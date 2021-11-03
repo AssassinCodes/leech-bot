7a04ad22570485600a21ac885c0c6cb38b441cbc
Assassin <45894525+AssassinCodes@users.noreply.github.com>
2021-11-03 22:20:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,11 @@ module.exports = client;
 // Global Variables
 client.commands = new Collection();
 client.slashCommands = new Collection();
-client.config = require("./config.json");
 
 // Initializing the project
 require("./handler")(client);
 
-client.login(client.config.token);
+client.login(process.env.TOKEN);
 
 process.on("unhandledRejection", (reason, p) => {
     console.log(" [antiCrash] :: Unhandled Rejection/Catch");
