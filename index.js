bc0e9b4dde59f34d937d34044b45c75d31d43dc5
Assassin <45894525+AssassinCodes@users.noreply.github.com>
2021-11-03 22:45:09 +0000

    Update index.js

--- index.js
+++ index.js
@@ -16,17 +16,17 @@ client.login(process.env.TOKEN);
 
 process.on("unhandledRejection", (reason, p) => {
     console.log(" [antiCrash] :: Unhandled Rejection/Catch");
-    console.log(reason, p);
+    //console.log(reason, p);
 });
 process.on("uncaughtException", (err, origin) => {
     console.log(" [antiCrash] :: Uncaught Exception/Catch");
-    console.log(err, origin);
+    //console.log(err, origin);
 });
 process.on("uncaughtExceptionMonitor", (err, origin) => {
     console.log(" [antiCrash] :: Uncaught Exception/Catch (MONITOR)");
-    console.log(err, origin);
+    //console.log(err, origin);
 });
 process.on("multipleResolves", (type, promise, reason) => {
     console.log(" [antiCrash] :: Multiple Resolves");
-    console.log(type, promise, reason);
+    //console.log(type, promise, reason);
 });
